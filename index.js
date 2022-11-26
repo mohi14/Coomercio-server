2bf4d3076bbf685586fc56178716de5a8d28ae9a
mohi14 <mohiuddinkhan1409@gmail.com>
2022-11-27 00:54:38 +0600

    payment getway for stripe payment is created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const port = process.env.PORT || 5000;
 
@@ -89,6 +89,23 @@ async function run() {
             const result = await bookingsCollection.insertOne(booking);
             res.send(result)
         })
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
 
         app.get('/bookings/:id', async (req, res) => {
             const id = req.params.id;
